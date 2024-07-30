d2fe000aff22d0c526df88fdb6b11f1441077f04
Francisco Rigon <franciscorigon@gmail.com>
2024-07-30 15:03:16 -0300

    Projeto Conta Banco

---
+++ tipos-variaveis/src/AboutMe.java
@@ -0,0 +1,19 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+
+public class AboutMe {
+    public static void main(String[] args) {
+        
+        Scanner sc = New Scanner(Syste)
+
+        String nome = args [0];
+        String sobrenome = args [1];
+        int idade = Integer.valueOf(args [2]);
+        double altura = Double.valueOf(args [3]);
+
+        System.out.println("Olá, me chamo " + nome + " " + sobrenome);
+        System.out.println("Tenho " + idade + " anos");
+        System.out.println("Minha altura é " + altura + "cm");
+    }
+}
